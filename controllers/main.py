50460f5c5169cad64660169c15c8d83d6f0716e0
Mohamed Nizar <nizaucsc@gmail.com>
2020-10-05 18:21:07 +0530

    implement ipn only

--- controllers/main.py
+++ controllers/main.py
@@ -58,8 +58,6 @@ class PayhereController(http.Controller):
         res = False
         post['cmd'] = '_notify-validate'
         reference = post.get('order_id')
-        # md5sig = post.values()
-        # print(md5sig)
         tx = None
         if reference:
             tx = request.env['payment.transaction'].sudo().search([('reference', '=', reference)])
@@ -69,7 +67,7 @@ class PayhereController(http.Controller):
             _logger.warning('received notification for unknown payment reference')
             return False
         payhere_url = tx.acquirer_id.payhere_get_form_action_url()
-        pdt_request = (post.get('status_code'))  # check for specific pdt param
+        pdt_request = post.get('md5sig')  # check for specific pdt param
         if pdt_request:
             # this means we are in PDT instead of DPN like before
             # fetch the PDT token
@@ -115,7 +113,6 @@ class PayhereController(http.Controller):
         _logger.info('Beginning Payhere DPN form_feedback with post data %s', pprint.pformat(post))  # debug
         try:
             res = self.payhere_validate_data(**post)
-            pprint.pformat(res)
         except ValidationError:
             _logger.exception('Unable to validate the Payhere payment')
         return werkzeug.utils.redirect('/payment/process')
